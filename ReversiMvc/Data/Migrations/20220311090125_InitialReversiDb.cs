e2f64e6b11f5097f8e076ad6e652285c7ca6550b
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-11 10:03:27 +0100

    Add migrations

---
+++ ReversiMvc/Data/Migrations/20220311090125_InitialReversiDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReversiMvc.Data.Migrations;
+
+public partial class InitialReversiDb : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Players",
+            columns: table => new
+            {
+                Guid = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                Victories = table.Column<int>(type: "int", nullable: false),
+                Losses = table.Column<int>(type: "int", nullable: false),
+                Draws = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Players", x => x.Guid);
+            });
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Players");
+    }
+}
