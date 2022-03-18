97ca69084fbf6ffeb4bddb177936eef0f718f67e
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-18 15:24:36 +0100

    Implement recaptcha for login page

---
+++ ReversiMvc/Security/IRecaptcha.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Net;
+
+namespace ReversiMvc.Security;
+
+public interface IRecaptcha
+{
+    public const string InvalidMessage = "De reCAPTCHA is ongeldig!";
+    
+    public string SiteKey { get; }
+    
+    /// <summary>
+    /// Validates te reCAPTCHA request.
+    /// </summary>
+    /// <param name="formCollection">The form collection.</param>
+    /// <param name="clientIp">The IP of the client.</param>
+    /// <returns>Whether the client passed the recaptcha check or not.</returns>
+    Task<bool> Validate(IFormCollection formCollection, IPAddress clientIp);
+}
---
+++ ReversiMvc/Security/RecaptchaConfiguration.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+namespace ReversiMvc.Security;
+
+public class RecaptchaConfiguration
+{
+    public string SiteKey { get; set; }
+    public string PrivateKey { get; set; }
+}
