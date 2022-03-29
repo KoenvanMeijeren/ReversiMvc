d56027349a87cf8ac8defa00c7e1d900a5b07773
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-29 13:26:06 +0200

    Implement player statistics

---
+++ ReversiMvc/Models/GameOverviewViewModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+namespace ReversiMvc.Models;
+
+public class GameOverviewViewModel
+{
+    public IEnumerable<GameEditViewModel>? Games { get; }
+
+    public PlayerEntity LoggedInPlayer { get; }
+
+    public GameOverviewViewModel(IEnumerable<GameEditViewModel>? games, PlayerEntity loggedInPlayer)
+    {
+        this.Games = games;
+        this.LoggedInPlayer = loggedInPlayer;
+    }
+}
